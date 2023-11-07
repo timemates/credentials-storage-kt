ac07dc2b5c62eab07581dcc04cbdd355d7756415
kotleni <yavarenikya@gmail.com>
2023-11-07 21:04:46 +0200

    migrated to multiplatform in utils

--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -1,16 +1,6 @@
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    id("java-library")
-    alias(libs.plugins.kotlin.jvm)
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-
-kotlin {
-    jvmToolchain(11)
+    alias(libs.plugins.kotlin.multiplatform)
 }
 
 dependencies {
